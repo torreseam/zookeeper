70c69ce696bf18c700925675d7bd980d275a3237
torreseam <615694479+torreseam@users_noreply.github.com>
2020-05-26 00:01:13 -0500

    Update Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const { animals } = require('./data/animals.json')
 
@@ -71,6 +72,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 //server listens to request and adds exact destination
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
