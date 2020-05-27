dc0f86268463a169826526498316616467d5a7d8
torreseam <615694479+torreseam@users_noreply.github.com>
2020-05-27 00:47:58 -0500

    update code pushed to insomnia for testing API

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming json data
+app.use(express.json());
+const { animals } = require('./data/animals');
 
 
-const { animals } = require('./data/animals.json')
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
+function createNewAnimal(body, animalsArray) {
+    //body is coming from req.body app.post
+    const animal = body;
+    //main function below
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, '/data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -45,31 +88,37 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// adding route to API animals & query
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
         res.json(result);
     } else {
-        res.send(404);
+        res.sendStatus(404);
     }
 });
 
-//server listens to request and adds exact destination
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
